f98efacfdc9c556213244d84486c2ae7d039408c
Беатрис <122818472+Beti28@users.noreply.github.com>
2023-11-18 22:57:45 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,69 @@
 import "./login.css"
-import dog4 from "./dog4.jpg"
+import { auth } from '../firebase-config';
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import dog4 from './dog4.jpg'
+
 export default function Login() {
+    const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  onAuthStateChanged(auth, (currentUser) =>{
+    setUser(currentUser)
+  })
+  const onLogin = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+    navigate('/');
+  };
+
     return (
-        <section id="login-page" className="auth">
-            <form id="login">
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" />
+      <div id="login-page">
+      <div className="loginForm">
+        <h1>Login</h1>
+        <form>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(event) => setLoginEmail(event.target.value)}
+            />
+          </div>
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <a href="/register">here</a></span>
-                        <img src={dog4} alt='' className="registerImg" />
-                    </p>
-                </div>
-            </form>
-        </section>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(event) => setLoginPassword(event.target.value)}
+            />
+          </div>
+          <button type="button" className='btnlogin' onClick={onLogin}>
+            Login
+          </button>
+          <div>
+            <p>
+              <img src={dog4} alt='' className="loginImg" />
+            </p>
+          </div>
+          <div>
+              <h4>User logged in:</h4>
+              {user?.email}
+          </div>
+        </form>
+      </div>
+    </div>
     );
 }
