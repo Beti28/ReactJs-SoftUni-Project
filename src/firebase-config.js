3e1535496aca29f9db513b22feed235d7cbba4e8
Беатрис <122818472+Beti28@users.noreply.github.com>
2023-11-18 22:59:01 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from '@firebase/firestore'
+import { getFirestore } from '@firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnGQwVDWTTfZ_sNcF4UZ3kIqrfg_YV9cQ",
-    authDomain: "reactjs-softuni-project.firebaseapp.com",
-    projectId: "reactjs-softuni-project",
-    storageBucket: "reactjs-softuni-project.appspot.com",
-    messagingSenderId: "229415181706",
-    appId: "1:229415181706:web:36bd6356551929336ebc73",
-    measurementId: "G-ETYGWXHVGS"
-  };
+  apiKey: "AIzaSyCnGQwVDWTTfZ_sNcF4UZ3kIqrfg_YV9cQ",
+  authDomain: "reactjs-softuni-project.firebaseapp.com",
+  projectId: "reactjs-softuni-project",
+  storageBucket: "reactjs-softuni-project.appspot.com",
+  messagingSenderId: "229415181706",
+  appId: "1:229415181706:web:36bd6356551929336ebc73",
+  measurementId: "G-ETYGWXHVGS"
+};
 
-  const app = initializeApp(firebaseConfig);
-
- export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
